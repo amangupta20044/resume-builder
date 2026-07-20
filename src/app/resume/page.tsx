433c1aec2a1af6f153644447fd573ce030676d53
amangupta20044 <a.mann20044@gmail.com>
2026-07-20 18:45:23 +0530

    feat: add authentication APIs and UI for login and registration
    - Implemented `registerApi` and `loginApi` in `auth.api.ts` for user registration and login.
    - Created `LoginPage` and `RegisterPage` components with forms for user authentication.
    - Integrated `react-hook-form` for form handling and validation.
    - Added Axios for API requests and Lucide icons for UI enhancement.
    - Established a new API route for fetching and updating resumes.
    - Created ATS score evaluation API to analyze resume content.
    - Added basic structure for the resume page.

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const page: React.FC<Props> = () => {
+    return (
+        <div>
+            this is my resume page
+        </div>
+    )
+}
+
+export default page
